3962ba8bdc8c7d1e75ef3ea2a850cff94c13667f
shilgupta <shilgupta@cs.stonybrook.edu>
2017-04-09 13:32:44 -0400

    modified code

--- src/vocab.py
+++ src/vocab.py
@@ -12,7 +12,8 @@ TO-DO till tuesday
 4. use standard/glove embeddings and try above 3 approches
 """
 
-def gen_vocab(filename):
+
+def gen_w2vmodel(filename):
     count = 0
     data_frame = pd.read_csv(filename)
     vocab = []
@@ -38,52 +39,64 @@ def gen_vocab(filename):
     w2v = dict(zip(model.index2word, model.syn0))
     model.save('embeddings')
 
-#
-def gen_features(filename):
+
+
+def calculate_dists(filename):
     model = gensim.models.Word2Vec.load('../models/embeddings')
+    count = 0
+    data_frame = pd.read_csv(filename)
+    with open("../data/distances.csv", 'a') as dist_file:
+        for index, row in data_frame.iterrows():
+            # print(count)
+            count += 1
+            q1 = row['question1']
+            q2 = row['question2']
+            true_label = int(row['is_duplicate'])
+
+            # python 2+
+            # q1_word = word_tokenize(q1.lower().decode('utf-8'))
+            # q2_word = word_tokenize(q2.lower().decode('utf-8'))
+
+            # python 3+
+            q1_word = word_tokenize(q1.lower())
+            q2_word = word_tokenize(q2.lower())
+
+            sum_q1 = np.zeros(100, )
+            sum_q2 = np.zeros(100, )
+            n = 0
+            for word in q1_word:
+                if word in model:
+                    n += 1
+                    sum_q1 += model[word]
+            if n != 0:
+                sum_q1 = sum_q1 / n
+
+            n = 0
+            for word in q2_word:
+                if word in model:
+                    n += 1
+                    sum_q2 += model[word]
+            if n != 0:
+                sum_q2 = sum_q2 / n
+
+            cos_distance = spatial.distance.cosine(sum_q1, sum_q2)
+            eucledian_distance = spatial.distance.euclidean(sum_q1, sum_q2)
+            dist_file.write(",".join(str(x) for x in [true_label, cos_distance, eucledian_distance]) + '\n')
+
+calculate_dists("../data/QuoraData.csv")
+
+def calculate_accuracy(distFile):
+    threshold_cos = 0
+    threshold_euc = 0
     tp = 0
     fp = 0
     tn = 0
     fn = 0
-    count = 0
-    data_frame = pd.read_csv(filename)
-    for index, row in data_frame.iterrows():
-        # print(count)
-        count += 1
-        q1 = row['question1']
-        q2 = row['question2']
-        true_label = int(row['is_duplicate'])
-
-        # python 2+
-        # q1_word = word_tokenize(q1.lower().decode('utf-8'))
-        # q2_word = word_tokenize(q2.lower().decode('utf-8'))
-
-        #python 3+
-        q1_word = word_tokenize(q1.lower())
-        q2_word = word_tokenize(q2.lower())
-
-        sum_q1 = np.zeros(100, )
-        sum_q2 = np.zeros(100, )
-        n = 0
-        for word in q1_word:
-            if word in model:
-                n += 1
-                sum_q1 += model[word]
-        sum_q1 = sum_q1 / n
-        n = 0
-        for word in q2_word:
-            if word in model:
-                n += 1
-                sum_q2 += model[word]
-        sum_q2 = sum_q2 / n
-        cos_distance = spatial.distance.cosine(sum_q1, sum_q2)
-        eucledian_distance = spatial.distance.euclidean(sum_q1, sum_q2)
-        print("************************************")
-        print(true_label)
-        print(cos_distance)
-        print(eucledian_distance)
-        threshold_cos = 0
-        threshold_euc = 0
+    with open(distFile, 'r') as dist_file:
+        for line in dist_file:
+            cols = line.split(",")
+            cos_distance = cols[1]
+            true_label = cols[0]
         if cos_distance > threshold_cos:
             if true_label == 0:
                 tn += 1
@@ -98,6 +111,3 @@ def gen_features(filename):
     print(" true negative :", tn)
     print(" flase positive :", fp)
     print(" false negative :", fn)
-
-
-gen_features("../data/QuoraData.csv")
